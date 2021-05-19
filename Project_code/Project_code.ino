96642ce69dddc93952b3d44233fa7ce087fad54b
Joey Ho <54887596+Joeyho229@users.noreply.github.com>
2021-05-19 11:04:17 +0200

    Constraints added
    Constraints for motors added

--- Project_code/Project_code.ino
+++ Project_code/Project_code.ino
@@ -7,27 +7,25 @@ int rotaryPin = A0; // select the input pin for the rotary
 int rotaryPin1 = 3;
 int rotaryValue = 0; // variable to store the value coming from the rotary
 int rotaryValue1 = 0;
+int pMin = 0;
+int pMax = 180;
 
 void setup() {
   Serial.begin(9600);
-  servo.attach(3);    // Tell the servo object that we've connected to pin 8
-  servo.write(0);     // Helps to adjust the initial position
-  servo1.attach(2);
-  servo1.write(0);
+  servo.attach(3);    // Tell the servo object that we've connected to pin 3
+  servo1.attach(2);   // Connectes the servo1 to pin 2
   delay(40);
 }
 
 void loop() {
   // read the value from the sensor:  
-
-  rotaryValue = map(rotaryValue, 0, 1023, 0, 255);
-  rotaryValue1 = map(rotaryValue1, 0, 1023, 0, 255);
   
-  rotaryValue = analogRead(rotaryPin);  // sets the value for the horizontal motor
-  rotaryValue1 = analogRead(rotaryPin1);    // Sets the value for the vertical motor
+  rotaryValue = analogRead(rotaryPin);      // sets the value for the vertical motor
+  rotaryValue1 = analogRead(rotaryPin1);    // Sets the value for the horizontal motor  
 
-  servo.write(rotaryValue);  // Set the position of the servo
-  servo1.write(rotaryValue1);
-  
-  //Serial.println(rotaryValue);
+  rotaryValue = constrain(rotaryValue, 0, 100);   // Uses constrain to keep its value
+  rotaryValue1 = constrain(rotaryValue1, 550, 1023);
+
+  servo.write(rotaryValue1);  // Position for servo
+  servo1.write(rotaryValue);  // Set the position of the servo1
 }
